c555ec53e63acab5f3abe89a5e42edce10ce1205
Kyle Ladd <kladd@barracuda.com>
2014-06-16 10:09:26 -0400

    New install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,23 @@
 DOTFILES_DIR=${HOME}/.dotfiles.d
 INSTALL_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Copy files
-for file in $(ls|grep -v "install.*\|README.*"); do
-    rm -r "$HOME/.$file"
-    cp -r "$PWD/$file" "$HOME/.$file"
+mkdir out
+
+for file in $(ls|grep -v "install.*\|README.*\|out"); do
+    cp -r "$PWD/$file" "out/.$file"
 done
+
+# Copy files
+pushd out
+rsync --exclude ".git/" \
+      --exclude ".DS_Store" \
+      --exclude "install.sh" \
+      --exclude "README.md" \
+      -avh --no-perms . ~
+popd
+rm -r out
+
+source ~/.bash_profile
 
 # Write dotfiles info
 echo "INSTALL_DIR=$INSTALL_DIR" > ${DOTFILES_DIR}/.dotfiles
