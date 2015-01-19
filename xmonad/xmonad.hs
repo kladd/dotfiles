1dc3c7df3ecaa8d26f8ac5b34de8ebe171578b4a
Kyle Ladd <kladd@barracuda.com>
2015-01-19 04:35:39 -0500

    AMEND ME: Xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,72 +1,135 @@
+    -- Base
+import XMonad
+import Data.Maybe (isJust)
+import XMonad.Config.Azerty
+import System.IO (hPutStrLn)
+import System.Exit (exitSuccess)
+import qualified XMonad.StackSet as W
+
+    -- Utilities
+import XMonad.Util.EZConfig (additionalKeysP, additionalMouseBindings)  
+import XMonad.Util.NamedScratchpad (NamedScratchpad(NS), namedScratchpadManageHook, namedScratchpadAction, customFloating)
+import XMonad.Util.Run (safeSpawn, unsafeSpawn, runInTerm, spawnPipe)
+import XMonad.Util.SpawnOnce
+
+    -- Hooks
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, defaultPP, dzenColor, pad, shorten, PP(..))
+import XMonad.Hooks.ManageDocks (avoidStruts, ToggleStruts(..))
+import XMonad.Hooks.Place (placeHook, withGaps, smart)
+import XMonad.Hooks.InsertPosition
+import XMonad.Hooks.FloatNext (floatNextHook, toggleFloatNext, toggleFloatAllNew)
+
+    -- Actions
+import XMonad.Actions.Promote
+import XMonad.Actions.RotSlaves (rotSlavesDown, rotAllDown)
+import XMonad.Actions.CopyWindow (kill1, copyToAll, killAllOtherCopies, runOrCopy)
+import XMonad.Actions.WindowGo (runOrRaise, raiseMaybe)
+import XMonad.Actions.WithAll (sinkAll, killAll)
+import XMonad.Actions.CycleWS (moveTo, shiftTo, WSType(..)) 
+import XMonad.Actions.GridSelect (GSConfig(..), goToSelected, bringSelected, colorRangeFromClassName, buildDefaultGSConfig)
+import XMonad.Actions.DynamicWorkspaces (addWorkspacePrompt, removeEmptyWorkspace)
+import XMonad.Actions.Warp (warpToWindow, banishScreen, Corner(LowerRight))
+import XMonad.Actions.MouseResize
+import qualified XMonad.Actions.ConstrainedResize as Sqr
+
+    -- Layouts modifiers
+import XMonad.Layout.PerWorkspace (onWorkspace) 
+import XMonad.Layout.Renamed (renamed, Rename(CutWordsLeft, Replace))
+import XMonad.Layout.WorkspaceDir 
+import XMonad.Layout.Spacing (spacing) 
+import XMonad.Layout.Minimize
+import XMonad.Layout.Maximize
+import XMonad.Layout.BoringWindows (boringWindows)
+import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
+import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
+import XMonad.Layout.Reflect (reflectVert, reflectHoriz, REFLECTX(..), REFLECTY(..))
+import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), Toggle(..), (??))
+import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
+import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
+
+    -- Layouts
+import XMonad.Layout.GridVariants (Grid(Grid))
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.OneBig
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.ZoomRow (zoomRow, zoomIn, zoomOut, zoomReset, ZoomMessage(ZoomFullToggle))
+import XMonad.Layout.IM (withIM, Property(Role))
+import XMonad.Layout.Gaps
+import XMonad.Layout.NoBorders
+
+    -- Prompts
+import XMonad.Prompt (defaultXPConfig, XPConfig(..), XPPosition(Top), Direction1D(..))
 
 import qualified Data.Map as M
-import System.IO
-
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Layout.IndependentScreens
-import XMonad.Layout.Spacing
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.Loggers
-import XMonad.Util.Run(spawnPipe)
 
 -------------------------------------------------------------------------------
 -- Colors and borders
 --
-myNormalBorderColor  = "#404040"
-myFocusedBorderColor = "#7C7C7C"
-
-xmobarTitleColor = "#FFB6B0"
-xmobarCurrentWorkspaceColor = "#CEFFAC"
+myFont          = "-*-terminus-*-*-*-*-14-*-*-*-*-*-*-*"
+myBorderWidth   = 1
+myColorBG       = "#151515"
+myColorWhite    = "#ebebeb"
+myColorRed      = "#C3143B"
+myColorGray     = "#545454"
+myColorDarkgray = "#353535"
 
 -------------------------------------------------------------------------------
 -- Workspaces
 -- The default number of workspaces (virtual screens) and their names.
 --
-myWorkspaces = ["1.term", "2.code", "3.web", "4.irc", "5.vm"] ++ map show [6..9]
+myWorkspaces = ["term", "code", "web", "irc", "vm"] ++ map show [6..9]
 
 -------------------------------------------------------------------------------
 -- dmenu
 --
 dmenu_command = concat
     [ "dmenu_run -i "
-    , "-fn \"xft:Terminus:pixelsize=11,style=italic\" "
-    , "-nb \"#000000\" "
-    , "-nf \"#FFFFFF\" "
-    , "-sb \"#CCEFAC\" "
-    , "-sf \"#000000\""
+    , "-fn \"" ++ myFont ++ "\" "
+    , "-nb \"" ++ myColorBG ++ "\" "
+    , "-nf \"" ++ myColorWhite ++ "\" "
+    , "-sb \"" ++ myColorRed ++ "\" "
+    , "-sf \"" ++ myColorWhite ++ "\" "
     ]
+myBitmapsDir = "/home/kladd/.xmonad/statusbar/icons"
+myXmonadBarL = "dzen2 -x '0' -y '0' -h '16' -w '1000' -ta 'l' -fg '"++myColorWhite++"' -bg '"++myColorBG++"' -fn '"++myFont++"' "
+myXmonadBarR = "conky -c /home/kladd/.xmonad/statusbar/conky_dzen | dzen2 -x '1000' -y '0' -w '920' -h '16' -ta 'r' -bg '"++myColorBG++"' -fg '"++myColorWhite++"' -fn '"++myFont++"'"
 
-dzen_left = concat
-    [ "dzen2 "
-    , "-fn 'xft:Terminus:pixelsize=11,style=italic' "
-    , "-ta 'l' "
-    , "-w '1920' "
-    ]
+myLogHook h  = dynamicLogWithPP $ defaultPP
+      { ppOutput           = hPutStrLn h
+      , ppCurrent          = dzenColor myColorWhite myColorRed . pad
+      , ppHidden           = dzenColor myColorGray myColorBG  . pad
+      , ppVisible          = dzenColor myColorWhite "" . pad
+      , ppHiddenNoWindows  = const ""
+      , ppOrder            = \(ws:l:t:_) -> [ws,l,t]
+      , ppSep              = dzenColor myColorRed myColorBG " | "
+      , ppTitle            = dzenColor myColorWhite myColorBG  . shorten 75
+      , ppWsSep            = dzenColor myColorRed myColorBG ""
+      , ppLayout           = dzenColor myColorWhite myColorBG  .
+                             (\x -> case x of
+                                 "oneBig"       -> "^i("++myBitmapsDir++"/mini/nbstack.xbm)"
+                                 "tiled"        -> "^i("++myBitmapsDir++"/mini/tile.xbm)"
+                                 "lined"        -> "^i("++myBitmapsDir++"/mini/bstack2.xbm)"
+                                 "monocle"      -> "^i("++myBitmapsDir++"/mini/monocle.xbm)"
+                                 "grid"         -> "^i("++myBitmapsDir++"/mini/grid.xbm)"
+                                 "float"        -> "^i("++myBitmapsDir++"/mini/float.xbm)"
+                                 "gimp"         -> "^i("++myBitmapsDir++"/fox.xbm)"
+                                 "Full"         -> "^i("++myBitmapsDir++"/mini/monocle2.xbm)"
+                                 _              -> x
+                             )
+      } where noScratchPad ws = if ws == "NSP" then "" else pad ws
 
-dzen_right = concat
-    [ "i3status | dzen2 "
-    , "-fn 'xft:Terminus:pixelsize=11,style=italic' "
-    , "-ta 'r' "
-    , "-w '920' "
-    , "-x '1000' "
-    ]
+myLayoutHook = gaps [(L, 30),(R,30)] $ spacing 30 $ avoidStruts $ layoutHook defaultConfig
+-- myLayoutHook =  spacing 30 $ avoidStruts $ layoutHook defaultConfig
 
 -------------------------------------------------------------------------------
 -- Keybindings
 --
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
-    [ ((modMask, xK_Return),
-        spawn $ XMonad.terminal conf)
-    , ((modMask, xK_p),
-        spawn dmenu_command)
-    , ((modMask, xK_l),
-        spawn "slimlock")
-    , ((0, 0x1008FF11),
-        spawn "amixer -q set Master 5%-")
-    , ((0, 0x1008FF13),
-        spawn "amixer -q set Master 5%+")
+    [ ((modMask, xK_Return), spawn $ XMonad.terminal conf)
+    , ((modMask, xK_p), spawn dmenu_command)
+    , ((modMask, xK_l), spawn "gnome-screensaver -l")
+    , ((0, 0x1008FF11), spawn "amixer -q set Master 5%-")
+    , ((0, 0x1008FF13), spawn "amixer -q set Master 5%+")
     ]
 
 -------------------------------------------------------------------------------
@@ -74,31 +137,21 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 --
 startup :: X ()
 startup = do
-    spawn "/usr/lib/gnome-settings-daemon/gnome-settings-daemon"
-    spawn "/usr/bin/compton"
-    spawn "/usr/bin/feh --bg-scale ~/linen.png"
-    spawn dzen_right
+    spawnOnce "/usr/lib/gnome-settings-daemon/gnome-settings-daemon &"
+    spawnOnce "/usr/bin/compton -r 6 -l -9 -t -9 -b -e 0.8 &"
 
 main = do
-    left <- spawnPipe dzen_left
+    dzenLeftBar  <- spawnPipe myXmonadBarL
+    dzenRightBar <- spawnPipe myXmonadBarR
     xmonad $ defaultConfig
         { terminal = "/usr/bin/gnome-terminal"
-        , workspaces = myWorkspaces
-        , normalBorderColor = myNormalBorderColor
-        , focusedBorderColor = myFocusedBorderColor
-        , keys = myKeys <+> keys defaultConfig
-        , layoutHook = spacing 5 $ avoidStruts $ layoutHook defaultConfig
-        , startupHook = startup
-        , borderWidth = 2
-        , modMask = mod4Mask
-        , logHook = dynamicLogWithPP $ dzenPP
-            { ppOutput = hPutStrLn left
-            , ppTitle = dzenColor xmobarTitleColor "" . shorten 100 . pad
-            , ppCurrent = dzenColor xmobarCurrentWorkspaceColor "" . pad
-            , ppVisible = dzenColor "#FFFFFF" "" . pad
-            , ppHidden = dzenColor "#AAAAAA" "" . pad
-            , ppHiddenNoWindows = const ""
-            , ppSep = "    "
-            , ppLayout = const ""
-            }
+        , borderWidth        = myBorderWidth
+        , focusedBorderColor = myColorWhite
+        , normalBorderColor  = myColorDarkgray
+        , keys               = myKeys <+> keys defaultConfig
+        , layoutHook         = myLayoutHook
+        , logHook            = myLogHook dzenLeftBar
+        , modMask            = mod4Mask
+        , startupHook        = startup
+        , workspaces         = myWorkspaces
         }
